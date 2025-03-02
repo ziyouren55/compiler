326a2041f9428c2b90e126ac9f13595ac159c268
ziyouren55 <231250122@smail.nju.edu.cn>
2025-03-02 22:46:33 +0800

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,43 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.*;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            System.exit(1);
+    public static void main(String[] args) throws IOException
+    {
+        if (args.length < 1) {
+            System.err.println("input path is required");
         }
+        String source = args[0];
+        CharStream input = CharStreams.fromFileName(source);
+        SysYLexer sysYLexer = new SysYLexer(input);
+
+        MyErrorListener errorListener = new MyErrorListener();
+        sysYLexer.removeErrorListeners();
+        sysYLexer.addErrorListener(errorListener);
+
+        List<?extends Token> tokens = sysYLexer.getAllTokens();
+
+        int index = 0;
+
+        if(errorListener.hasErrors())
+        {
+            errorListener.printLexerErrorInformation();
+        }
+        else
+        {
+            for (Token token : tokens)
+            {
+                String tokenStr = SysYLexer.VOCABULARY.getSymbolicName(token.getType()) + " " + token.getText() + " at Line " + token.getLine() + ".";
+                System.err.println(tokenStr);
+            }
+        }
+
     }
+
+
 }
---
+++ src/MyErrorListener.java
@@ -0,0 +1,38 @@
+import org.antlr.v4.runtime.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyErrorListener extends BaseErrorListener
+{
+    private final List<String> errors = new ArrayList<>();
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
+    {
+        String errorMessage;
+        if (msg.startsWith("token recognition error at:"))
+        {
+            errorMessage = String.format("Mysterious character \"%s\"" + ".", msg.substring(29,msg.length() - 1));
+        }
+        else
+        {
+            errorMessage = msg;
+        }
+        errors.add(String.format("Error type A at Line %d: %s", line, errorMessage));
+    }
+
+    public boolean hasErrors()
+    {
+        return !errors.isEmpty();
+    }
+
+    public void printLexerErrorInformation()
+    {
+        for (String error : errors)
+        {
+            System.err.println(error);
+        }
+    }
+}
+

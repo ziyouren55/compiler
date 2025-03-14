5e5149e68b5620a548b1ec64a13f112d5b933b80
ziyouren55 <231250122@smail.nju.edu.cn>
2025-03-14 21:56:21 +0800

    Changes

--- src/Main.java
+++ src/Main.java
@@ -32,11 +32,29 @@ public class Main {
         {
             for (Token token : tokens)
             {
-                String tokenStr = SysYLexer.VOCABULARY.getSymbolicName(token.getType()) + " " + token.getText() + " at Line " + token.getLine() + ".";
+                String text = token.getText();
+                if(token.getType() == SysYLexer.INTEGER_CONST)
+                {
+                    text = convertToDecimal(text);
+                }
+                String tokenStr = SysYLexer.VOCABULARY.getSymbolicName(token.getType()) + " " + text + " at Line " + token.getLine() + ".";
                 System.err.println(tokenStr);
             }
         }
 
+    }
+
+    public static String convertToDecimal(String input) {
+        if (input.startsWith("0x") || input.startsWith("0X")) {
+            // 十六进制转换为十进制，并返回字符串
+            return String.valueOf(Integer.parseInt(input.substring(2), 16));
+        } else if (input.startsWith("0") && input.length() > 1) {
+            // 八进制转换为十进制，并返回字符串
+            return String.valueOf(Integer.parseInt(input.substring(1), 8));
+        } else {
+            // 十进制数保持原样，并返回字符串
+            return input;  // 因为已经是十进制格式，直接返回原始字符串
+        }
     }
 
 

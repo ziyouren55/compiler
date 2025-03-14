1a00ee498b6382a17b676a2f591039e0a363e1b2
ziyouren55 <231250122@smail.nju.edu.cn>
2025-03-14 13:20:34 +0800

    Changes

--- src/Main.java
+++ src/Main.java
@@ -37,9 +37,9 @@ public class Main {
         }
         else
         {
-            parserVisitor formatter = new parserVisitor();
-            String formattedCode = formatter.visit(tree);
-            System.out.println(formattedCode);
+//            parserVisitor formatter = new parserVisitor();
+//            String formattedCode = formatter.visit(tree);
+//            System.out.println(formattedCode);
             //shan
             //System.out.println(check(formattedCode));
         }

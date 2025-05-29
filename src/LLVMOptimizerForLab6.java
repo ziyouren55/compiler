10dc8fb21ba9fd7a266c7e06a6daa42c57041d64
ziyouren55 <231250122@smail.nju.edu.cn>
2025-05-29 15:09:02 +0800

    Changes

--- src/LLVMOptimizerForLab6.java
+++ src/LLVMOptimizerForLab6.java
@@ -64,13 +64,13 @@ public class LLVMOptimizerForLab6 {
             boolean changed1 = constantPropagation.run();
 
             // 应用死代码消除优化（使用常量传播的结果）
-            boolean changed2 = deadCodeElimination.run();
+//            boolean changed2 = deadCodeElimination.run();
 
             // 应用未使用变量消除优化
-            boolean changed3 = unusedVarElimination.run();
+//            boolean changed3 = unusedVarElimination.run();
 
             // 检查是否有任何变化
-            changed = changed1 || changed2|| changed3;
+            changed = changed1;
 
             // 更新每个优化器的模块引用
             if (changed) {
